a7479eae524599e206083bc59d5f4206b5cec43d
Lukhnos Liu <lukhnos@lukhnos.org>
2017-05-16 18:40:03 -0700

    Update the script for getting Fira fonts

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FIRA_DEST=$PWD/Common/FiraFonts
-FIRA_ZIP=FiraFonts3111.zip
-FIRA_URL=http://www.carrois.com/wordpress/downloads/fira_3_1/$FIRA_ZIP
+FIRA_ZIP=4.202.zip
+FIRA_URL=https://github.com/mozilla/Fira/archive/$FIRA_ZIP
 
 echo Installing Fira fonts
 mkdir -p "$FIRA_DEST"
@@ -12,7 +12,7 @@ FIRA_TMP=$PWD/temp-$RANDOM
 if [ ! -e "$FIRA_ZIP" ]
 then
 	echo Obtaining $FIRA_URL
-	curl -O "$FIRA_URL" 
+	curl -LO "$FIRA_URL" 
 fi
 echo Unzipping the archive
 unzip -nq "$FIRA_ZIP" -d "$FIRA_TMP"
